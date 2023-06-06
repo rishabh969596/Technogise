3564b76a19e87c87c35c6ac731a461623c5d0752
Rishabh <shubhamgupta9695@gmail.com>
2023-06-07 00:10:30 +0530

    Initial commit

---
+++ src/main/java/com/technogise/LibraryManagementApp.java
@@ -0,0 +1,7 @@
+package com.technogise;
+
+public class LibraryManagementApp {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/com/technogise/LibraryManagementAppTest.java
@@ -0,0 +1,14 @@
+package com.technogise;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LibraryManagementAppTest
+{
+    @Test
+    public void test()
+    {
+        assertEquals(2,2);
+    }
+}
